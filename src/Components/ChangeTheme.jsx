8e41417569db3a8951f75ed328dd15de472de782
Jose Torin <joseptl551@gmail.com>
2021-08-18 19:34:40 -0400

    scrollreveal

--- src/Components/ChangeTheme.jsx
+++ src/Components/ChangeTheme.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../Context/ThemeContext";
 import styled from "styled-components";
 import { secondaryColor, secondaryDarkColor } from "../Colors/colors";
